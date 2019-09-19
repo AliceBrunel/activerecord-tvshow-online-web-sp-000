082e51febbbb356d2c28742da57c54d5d84145f8
Alice Brunel <alc.brunel@gmail.com>
2019-09-19 10:02:31 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,8 +1,8 @@
 class CreateShows < ActiveRecord::Migration[4.2]
   def change
     create_table :shows do |s|
-      t.string :name
-      t.string :network
+      s.string :name
+      s.string :network
       t.string :day 
       t.integer :rating 
     end
